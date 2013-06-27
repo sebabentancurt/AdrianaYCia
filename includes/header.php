1cd01d3d27440a369db33c99363f33a6e4d7549b
Sebastian Bentancurt <sebabentancurt@gmail.com>
2013-06-27 04:49:21 -0300

    Pre Header

--- includes/header.php
+++ includes/header.php
@@ -11,8 +11,8 @@
         <div id="cabecera">
         <header>
             <div id="headerCenter">
-                <label  id="logo">Adriana Y Cia </label>
-                <input type:"search" name="search" placeholder="Escriba producto a buscar" class="searchInput">
+                <label  id="logo"><a href="/index.php">Adriana Y Cia</a></label>
+                <input type="search" name="search" placeholder="Buscar producto" autocomplete="off" class="searchInput">
            
                 
             </div>
@@ -20,15 +20,14 @@
 
              <nav>
                 <ul>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/SEO" target="_blank">Griferías</a></li>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/tutoriales" target="_blank">Cerámicas</a></li>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/crear%20web" target="_blank">Pinturas</a></li>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/css3" target="_blank">Art. Baños</a></li>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/dise%C3%B1o%20web" target="_blank">Art. Cocina</a></li>
-                    <li><a href="http://miopiblog.blogspot.com.es/search/label/google" target="_blank">Jardineria</a></li>
+                    <li><a href="/index.php">Griferías</a></li>
+                    <li><a href="/index.php">Cerámicas</a></li>
+                    <li><a href="/index.php">Pinturas</a></li>
+                    <li><a href="/index.php">Art. Baños</a></li>
+                    <li><a href="/index.php">Art. Cocina</a></li>
+                    <li><a href="/index.php">Jardineria</a></li>
                     <li style="border-right: 0px"><a href="http://miopiblog.blogspot.com.es/2013/02/seorimicuaro-que-es-y-para-que-sirve.html" target="_blank">Otros</a></li>
                 </ul>
             </nav>
        </div>
-        
    
