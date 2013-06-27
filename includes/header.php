cf8dfdd7340b0a485e5eee848fe954d308ecd98d
Sebastian Bentancurt <sebabentancurt@gmail.com>
2013-06-27 03:17:23 -0300

    Header casi

--- includes/header.php
+++ includes/header.php
@@ -11,14 +11,23 @@
         <div id="cabecera">
         <header>
             <div id="headerCenter">
-                <label id="logo">Adriana Y Cia </label>
-                <input type="search" name="search" placeholder="Escriba producto a buscar" class="searchInput">
+                <label  id="logo">Adriana Y Cia </label>
+                <input type:"search" name="search" placeholder="Escriba producto a buscar" class="searchInput">
+           
                 
             </div>
        </header>
 
              <nav>
-                Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia | Griferia 
+                <ul>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/SEO" target="_blank">Griferías</a></li>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/tutoriales" target="_blank">Cerámicas</a></li>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/crear%20web" target="_blank">Pinturas</a></li>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/css3" target="_blank">Art. Baños</a></li>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/dise%C3%B1o%20web" target="_blank">Art. Cocina</a></li>
+                    <li><a href="http://miopiblog.blogspot.com.es/search/label/google" target="_blank">Jardineria</a></li>
+                    <li style="border-right: 0px"><a href="http://miopiblog.blogspot.com.es/2013/02/seorimicuaro-que-es-y-para-que-sirve.html" target="_blank">Otros</a></li>
+                </ul>
             </nav>
        </div>
         
--- index.php
+++ index.php
@@ -6,24 +6,7 @@
 
 
     <div id="main">
-    <h3>Get started with your website</h3>
-
-    <ol class="round">
-        <li class="one">
-            <h5>Login as admin </h5>
-           The site admin username and password are stored in the config file in the Includes directory. 
-        </li>
-        <li class="two">
-            <h5>Customize your site</h5>
-             After you login, you can add, delete, and modify web pages.
-         </li>
-        <li class="asterisk">
-            <div class="visit">
-                To learn more about PHP, visit <a href="http://php.net" title="PHP.net Website">http://php.net</a>. 
-            </div>
-         </li>
-    </ol>
-
+    
 
     </div>
 
